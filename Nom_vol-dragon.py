5949a96a71b0a9e1282d39092ac3ad2b3d6f5b24
Gabriel <gabriel@mazure.fr>
2023-01-13 18:15:54 +0100

    Idle flying and mouse control

--- Nom_vol-dragon.py
+++ Nom_vol-dragon.py
@@ -1,14 +1,5 @@
 """
-Vol de dragon en python avec biblio Pygame
-Copier si nécessaire le dossier PIL contenant la bibliothèque PIL dans le dossier C:\Program Files (x86)\Thonny\Lib\site-packages
-Créer par F HERGNIOT complété par ..........  
-Ouvrir et analyser le fichier wyvern_vol.png. Déterminer la taille de découpe de chaque image.
-Analyser le programme en cours en le commentant derrière #
-Utiliser la bibliothèque Image de PIL pour découper le fichier  wyvern_vol.png en une suite de 8x8 images 
-Mettre ces images dans des listes en fonction du déplacement vers la gauche, droite, haut, nord-est ....
-En fonction de l'appui sur les touches du clavier numérique déplacer le dragon selon la direction ( 4 à gauche 8 en haut 7 nord-est...)
-en changeant les 8 images de la liste correspondante pour donner l'impression de vol.
-Si aucune touche est appuyée, il fera du surplace.
+
 
 Bonus1: Lancer une boule de feu si on appui sur la touche espace
 Bonus2: Modifier le programme pour que le dragon se déplace et rejoigne le clic de la souris en calculant l'angle et distance entre dragon et souris
@@ -25,12 +16,20 @@ from PIL import Image  #
 (largeur, hauteur) = (1000, 800)  # definit la hauteur et la largeur de la fenêtre de l'application
 colorWhite=(255,255,255)   #On définit la couleur blanche
 colorRed=(255,0,0)   #On définit la couleur rouge
-x=400
-y=300
+x=402
+y=302
 FPS = 60
 score=0
 j=0
+directionFacing = 0
+i = 0
+clockAutoMovement = 0
+coords = [x+ 256/2,y +256/2]
 
+objectiveX = 0
+objectiveY = 0
+obj = [objectiveX,objectiveY]
+goingToObjective = False
 
 dragonGauche=[]  # Liste des 8 images du dragon se déplaçant vers la gauche.
 dragonGaucheHaut=[]
@@ -122,108 +121,154 @@ dragon=dragonGauche[2] # Initialise la première image du dragon
 def deplacementGauche():  # Fonction pour déplacer le dragon vers la gauche
     global x, dragon,j
     x=x-10
-    dragon=dragonGauche[j]   # à compléter pour changer d'image lors du déplacement
-    j+=1
-    if j == 8:
-        j = 0
+    
+
 
 
 def deplacementDroite():  
     global x, dragon,j
     x=x+10
-    dragon=dragonDroite[j]   
-    j+=1
-    if j == 8:
-        j = 0
+    
 
 def deplacementHaut():
     global y, dragon,j
     y=y-10
-    dragon=dragonHaut[j]  
-    j+=1
-    if j == 8:
-        j = 0
+    
 
 def deplacementBas():  
     global y, dragon,j
     y=y+10
-    dragon=dragonBas[j]   
-    j+=1
-    if j == 8:
-        j = 0
+    
 
 def deplacementGaucheHaut():  
     global y, x, dragon,j
     y=y-10
     x-=10
-    dragon=dragonGaucheHaut[j]   
-    j+=1
-    if j == 8:
-        j = 0
+    
 
 def deplacementDroiteHaut():  
     global y, x, dragon,j
     y=y-10
     x+=10
-    dragon=dragonDroiteHaut[j]   
-    j+=1
-    if j == 8:
-        j = 0
+    
 
 def deplacementDroiteBas():  
     global y, x, dragon,j
     y=y+10
     x+=10
-    dragon=dragonDroiteBas[j]   
-    j+=1
-    if j == 8:
-        j = 0
+    
 
 def deplacementGaucheBas():  
     global y, x, dragon,j
     y=y+10
     x-=10
-    dragon=dragonGaucheBas[j]   
-    j+=1
-    if j == 8:
-        j = 0
-
+    
 
 #BOUCLE INFINIE
 continuer = True
 while continuer:
     clock.tick(FPS)#Précise le nombre d'image par seconde Frame par seconde
-
-    #j+=1
-    #if j == 8:
-    #    j = 0
-
+    coords = [x+ 256/2,y+ 256/2]
+    
+    clockAutoMovement +=1
+    if clockAutoMovement == 2:
+        clockAutoMovement =0
+    
+    obj = [objectiveX,objectiveY]
+    while obj[0]%10 != 0:
+        obj[0] +=1
+    while obj[1]%10 != 0:
+        obj[1] +=1
+        
+    mouseX, mouseY =  pygame.mouse.get_pos()
+    
+    
+    dragon = directions[directionFacing][j]
+    i+=1
+    if i > 4: 
+        j += 1
+        if j == 8:
+            j = 0
+        i = 0
     for event in pygame.event.get() :   #On parcours la liste de tous les événements reçus
         if event.type == QUIT:     #Si un de ces événements est de type QUIT (Alt+F4) ou bouton fermeture
-            continuer = False      #On arrête la boucle
+            continuer = False      
         if event.type == KEYDOWN :  # Si touche appuyée
             if event.key == K_KP1: # Si touche clavier numérique 1
                 deplacementGaucheBas()
-            if event.key == K_KP2: # Si touche clavier numérique 2
+                directionFacing = 2
+            elif event.key == K_KP2: # Si touche clavier numérique 2
                 deplacementBas()
-            if event.key == K_KP3: # Si touche clavier numérique 3
+                directionFacing = 6
+            elif event.key == K_KP3: # Si touche clavier numérique 3
                 deplacementDroiteBas()
-            if event.key == K_KP4: # Si touche clavier numérique 4
+                directionFacing = 5
+            elif event.key == K_KP4: # Si touche clavier numérique 4
                 deplacementGauche()
-            if event.key == K_KP6: # Si touche clavier numérique 6
+                directionFacing= 0
+            elif event.key == K_KP6: # Si touche clavier numérique 6
                 deplacementDroite()
-            if event.key == K_KP7: # Si touche clavier numérique 7
+                directionFacing= 3
+            elif event.key == K_KP7: # Si touche clavier numérique 7
                 deplacementGaucheHaut()
-            if event.key == K_KP8: # Si touche clavier numérique 8
+                directionFacing = 1
+            elif event.key == K_KP8: # Si touche clavier numérique 8
                 deplacementHaut()
-            if event.key == K_KP9: # Si touche clavier numérique 9
+                directionFacing = 7
+            elif event.key == K_KP9: # Si touche clavier numérique 9
                 deplacementDroiteHaut()
+                directionFacing = 4
+            elif event.key == K_KP5:
+                if goingToObjective == True:
+                    print("Stopped automatic movement by pressing numPad5")
+                goingToObjective = False
+            
+            elif event.key == K_c:
+                print("Dragon coordinates =", coords)
+            elif event.key == K_o:
+                print("Objective coordinates =", obj)
 
-                
-                
 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print("MouseX =",mouseX)
+            print("MouseY =",mouseY)
+            if goingToObjective == False:
+                objectiveX = mouseX
+                objectiveY = mouseY
+                goingToObjective = True
+            print("dragonX =", x)
+            print("dragonY =", y)
+    
+    if goingToObjective == True and coords == obj:
+        goingToObjective = False
+        print("Reached destination and stopped")
               
-                
+    if goingToObjective == True and clockAutoMovement == 0:
+        if coords[0] < obj[0] and coords[1] < obj[1]:
+            deplacementDroiteBas()
+            directionFacing = 5
+        elif coords[0] > obj[0] and coords[1] > obj[1]:
+            deplacementGaucheHaut()
+            directionFacing = 1
+        elif coords[0] > obj[0] and coords[1] < obj[1]:
+            deplacementGaucheBas()
+            directionFacing = 2
+        elif coords[0] < obj[0] and coords[1] > obj[1]:
+            deplacementDroiteHaut()
+            directionFacing = 4 
+        else:
+            if coords[0] < obj[0] and coords[1] == obj[1]:
+                deplacementDroite()
+                directionFacing = 3
+            elif coords[0] > obj[0] and coords[1] == obj[1]:
+                deplacementGauche()
+                directionFacing = 0
+            elif coords[0] == obj[0] and coords[1] > obj[1]:
+                deplacementHaut()
+                directionFacing = 7
+            elif coords[0] == obj[0] and coords[1] < obj[1]:
+                deplacementBas()
+                directionFacing = 6
                 
     
     fenetre.fill(colorWhite) # efface l'image    
